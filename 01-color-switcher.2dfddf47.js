1b8d936b06b1a917e005b23a394a64d042231cc9
IG00RA <113199658+IG00RA@users.noreply.github.com>
2023-03-01 14:21:29 +0000

    Deploying to gh-pages from @ IG00RA/goit-js-hw-09@5ba16693b80fb1375420f22b9c115917b18dbd09 🚀

---
+++ 01-color-switcher.2dfddf47.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.body,startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(o){var n="#".concat(Math.floor(16777215*Math.random()).toString(16));t.body.style.backgroundColor=n})),t.stopBtn.addEventListener("click",(function(t){}))}();
+//# sourceMappingURL=01-color-switcher.2dfddf47.js.map
