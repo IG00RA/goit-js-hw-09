c840700d8305aaa49648557fb37c54e2db21c0ab
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-11-08 08:28:30 +0200

    add ts config

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,37 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PromiseGenerator from './promis-generator';
 const promiseGenerator = new PromiseGenerator();
-
 const refs = {
-  button: document.querySelector('button[type="submit"]'),
-  form: document.querySelector('.form'),
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
+    button: document.querySelector('button[type="submit"]'),
+    form: document.querySelector('.form'),
+    delay: document.querySelector('input[name="delay"]'),
+    step: document.querySelector('input[name="step"]'),
+    amount: document.querySelector('input[name="amount"]'),
 };
-
 let position = 0;
-
 const onFormSubmit = event => {
-  event.preventDefault();
-  let amount = refs.amount.value;
-  const step = Number(refs.step.value);
-  let delay = Number(refs.delay.value);
-  while (amount > 0) {
-    amount -= 1;
-    position++;
-    promiseGenerator
-      .createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delay += step;
-  }
-  position = 0;
-  refs.button.disabled = true;
-  refs.form.reset();
-  setTimeout(() => {
-    refs.button.disabled = false;
-  }, 2000);
+    event.preventDefault();
+    let amount = refs.amount.value;
+    const step = Number(refs.step.value);
+    let delay = Number(refs.delay.value);
+    while (amount > 0) {
+        amount -= 1;
+        position++;
+        promiseGenerator
+            .createPromise(position, delay)
+            .then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+            .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+        delay += step;
+    }
+    position = 0;
+    refs.button.disabled = true;
+    refs.form.reset();
+    setTimeout(() => {
+        refs.button.disabled = false;
+    }, 2000);
 };
-
 refs.form.addEventListener('submit', onFormSubmit);
---
+++ src/ts/01-color-switcher.ts
@@ -0,0 +1,42 @@
+function getRandomHexColor(): string {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+interface Refs {
+  body: HTMLBodyElement | null;
+  startBtn: HTMLButtonElement | null;
+  stopBtn: HTMLButtonElement | null;
+}
+
+const refs: Refs = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+refs.stopBtn.disabled = true;
+let switcherID;
+
+function onStartBtnClick() {
+  refs.stopBtn.disabled = false;
+  refs.startBtn.disabled = true;
+  if (refs.body.classList.contains('cls')) {
+    return;
+  }
+
+  refs.body.classList.add('cls');
+
+  switcherID = setInterval(() => {
+    const backgroundColor = getRandomHexColor();
+    refs.body.style.backgroundColor = backgroundColor;
+  }, 1000);
+}
+function onStopBtnClick() {
+  clearInterval(switcherID);
+  refs.body.classList.remove('cls');
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+}
